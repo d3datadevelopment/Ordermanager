6148db71609a1cad8de41bfbed1c5b289a19ef8f
Daniel Seifert <ds@shopmodule.com>
2023-05-03 14:58:32 +0200

    add 5.1.1.3 Source Guardian PHP 8

--- src/Modules/Application/Model/d3_oxemail_ordermanager.php
+++ src/Modules/Application/Model/d3_oxemail_ordermanager.php
@@ -697,6 +697,7 @@ class d3_oxemail_ordermanager extends d3_oxemail_ordermanager_parent
                 $oShop->__get('oxshops__oxname')->getRawValue()
             );
         } else {
+            $this->setFrom($oShop->getFieldData('oxinfoemail'), $oShop->__get('oxshops__oxname')->getRawValue());
             $this->setReplyTo($oShop->getFieldData('oxinfoemail'), $oShop->__get('oxshops__oxname')->getRawValue());
         }
     }
